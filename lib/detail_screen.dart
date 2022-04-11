fdfa1b3ba88933c63ea9539c975b55cc34d1da46
Tachi <nomuramtr75@gmail.com>
2022-04-11 22:24:49 +0700

    upgrade package

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:aplikasi_travel/model/tourism_place.dart';
 
